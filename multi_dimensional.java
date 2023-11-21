39bb150a234349ae7656515b4d7507c6b63f05ab
Megharaja MJ <123971550+Meghamj01@users.noreply.github.com>
2023-11-21 17:48:42 +0530

    Add files via upload

---
+++ multi_dimensional.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class multi_dimensional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int x[] = {1,2,3,4,5,6,7};
+		
+	//	System.out.println("length of the first string is " + x[0].length());
+		
+		
+		int x[][][]= {{{10,20,30},{40,50,60}}, {{70,80},{90,100,110}},{{120,130}}};
+		System.out.println(x[0] [1] [2]);
+		System.out.println(x[1] [1] [1]);
+		System.out.println(x[1] [0] [1]);
+		System.out.println(x[2] [0] [1]);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ single_dimensional.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class single_dimensional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[] = {1,2,3};
+		System.out.println(a.length);
+		
+		
+	}
+
+}
